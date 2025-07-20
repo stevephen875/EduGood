19449b7e9f245ed290ee40d1d06c6111d10a4311
stevephen875 <stevephen457@gmail.com>
2025-07-20 16:00:11 +0700

    commit

---
+++ app/src/main/java/com/example/edugood/Mandarin2Materi1Activity.java
@@ -0,0 +1,46 @@
+package com.example.edugood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class Mandarin2Materi1Activity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private Button btnQuiz;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mandarin2_materi1);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("EduGood");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        btnQuiz = findViewById(R.id.btn_quiz);
+        btnQuiz.setOnClickListener(v -> {
+            Intent intent = new Intent(Mandarin2Materi1Activity.this, Mandarin2Quiz1Activity.class);
+            startActivity(intent);
+
+        });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
--- app/src/main/java/com/example/edugood/MandarinKelas2Activity.java
+++ app/src/main/java/com/example/edugood/MandarinKelas2Activity.java
@@ -1,12 +1,38 @@
 package com.example.edugood;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MandarinKelas2Activity extends AppCompatActivity {
+
+    private Button btnMateri1, btnMateri2, btnMateri3, btnFinalTest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mandarin_kelas2); //
+        setContentView(R.layout.activity_mandarin_kelas2);
+
+        btnMateri1 = findViewById(R.id.btn_materi1);
+        btnMateri2 = findViewById(R.id.btn_materi2);
+        btnMateri3 = findViewById(R.id.btn_materi3);
+        btnFinalTest = findViewById(R.id.btn_final_test);
+
+        btnMateri1.setOnClickListener(v -> {
+            Intent intent = new Intent(MandarinKelas2Activity.this, Mandarin2Materi1Activity.class);
+            startActivity(intent);
+        });
+
+        btnMateri2.setOnClickListener(v -> {
+            Intent intent = new Intent(MandarinKelas2Activity.this, Mandarin2Materi2Activity.class);
+            startActivity(intent);
+        });
+
+        btnMateri3.setOnClickListener(v -> {
+            Intent intent = new Intent(MandarinKelas2Activity.this, Mandarin2Materi3Activity.class);
+            startActivity(intent);
+        });
+
     }
 }
